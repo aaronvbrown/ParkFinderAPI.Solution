90d83dbb668a941370e6d2f1f11ede1c19aa975d
Aaron Brown <worldofaaron@hotmail.com>
2023-11-26 08:57:54 -0800

    Pagination Started

--- ParkFinderAPI/Controllers/ParksController.cs
+++ ParkFinderAPI/Controllers/ParksController.cs
@@ -14,9 +14,10 @@ namespace ParkFinderAPI.Controllers
       _db = db;
     }
 
-    // GET api/animals
+    // GET api/parks
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Park>>> Get(string name, string state, string type)
+    // public async Task<ActionResult<IEnumerable<Park>>> Get(string name, string state, string type)
+    public async Task<ActionResult<IEnumerable<Park>>> Get(ParkParameters parkParameters, string name, string state, string type)
     {
       IQueryable<Park> query = _db.Parks.AsQueryable();
 
@@ -36,7 +37,9 @@ namespace ParkFinderAPI.Controllers
       }
 
 
-      return await query.ToListAsync();
+      return await query
+                    .Skip((parkParameters.PageNumber -1) * parkParameters.PageSize)
+                    .ToListAsync();
     }
 
     // GET: api/Parks/5
